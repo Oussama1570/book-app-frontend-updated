6ee17817b719723f6cb173c108cde60e1afe37fb
Oussama <Messamessus@yahoo.com>
2025-01-28 18:19:47 +0100

    update getbaseurl

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,12 @@
+// utils/baseURL.js
 const getBaseUrl = () => {
-    return "https://book-app-backend-env-updated.vercel.app/"
-}
+    if (process.env.NODE_ENV === 'development') {
+        return 'http://localhost:5000'; // Or your development server URL
+    } else {
+        return 'https://book-app-backend-env-updated.vercel.app/';
+    }
+};
 
 export default getBaseUrl;
+
+
